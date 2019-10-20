9362a423379a1e64699f458580da46b6697e1755
RhioSutoyo <RhioSutoyo@DESKTOP-8H28IOI>
2019-10-20 17:10:17 +0700

    final

--- TestProject/src/GetScopusDataOnSINTA.java
+++ TestProject/src/GetScopusDataOnSINTA.java
@@ -28,7 +28,14 @@ public class GetScopusDataOnSINTA {
 		// FILL THIS
     	int universityID 	= 388;
     	int startPage 		= 1;
-    	int endPage 		= 1;//getEndPage(universityID);
+    	int endPage 		= getEndPage(universityID);
+    	
+    	// NAMA KOLOM
+		String kolom = new String("Quartile,Paper Title,"
+				+ "Indexed By,Volume,Issue,Publication Date,"
+				+ "Publication Type,Cited By");
+		System.out.println(kolom);
+    	fileWriter.write(kolom+"\n");
     	
     	for(pageNumber = startPage; pageNumber <= endPage ; pageNumber++ ) {
 			// ACCESS THE WEB PAGE
@@ -44,12 +51,30 @@ public class GetScopusDataOnSINTA {
 	        		endIndex = inputLine.indexOf("<",startIndex);
 	        		tempString = inputLine.substring(startIndex, endIndex).trim();
 	        		if(tempString.contains("Q") || tempString.contains("-")) {
-		        		System.out.println(tempString);
+//		        		System.out.println(tempString);
 		        		newScopus.setQuartile(tempString);
 	        		}
 	        		else {
-	        			System.out.println(tempString);
+//	        			System.out.println(tempString);
 		        		newScopus.setPublicationsCitation(tempString);
+		        				        		
+		        		// COMBINE AND STORE RESULT
+		        		finalResult += newScopus.getQuartile() + ",";
+		        		finalResult += newScopus.getPaperTitle() + ",";
+		        		finalResult += newScopus.getPublicationsName() + ",";
+		        		if(newScopus.getVolume().isEmpty()) finalResult += "-,";
+		        		else finalResult += newScopus.getVolume() + ",";
+		        		if(newScopus.getIssue().isEmpty()) finalResult += "-,";
+		        		else finalResult += newScopus.getIssue() + ",";
+		        		finalResult += newScopus.getDate() + ",";
+//		        		if(newScopus.getPublicationsJournal().equals("Conference Proceedin"))
+//		        			finalResult += "Conference Proceeding,";
+//		        		else 
+		        			finalResult += newScopus.getPublicationsJournal() + ",";
+		        		finalResult += newScopus.getPublicationsCitation();
+
+		        		System.out.println(finalResult);
+		        		fileWriter.write(finalResult + "\n");		        		
 	        		}
 				}
 				// Paper Title
@@ -57,11 +82,20 @@ public class GetScopusDataOnSINTA {
 					startIndex = inputLine.indexOf(">", 2) + 1;
 					endIndex = inputLine.indexOf("<", startIndex);
 					tempString = inputLine.substring(startIndex, endIndex).trim();					
-					System.out.println(tempString);
-					newScopus.setPaperTitle(tempString);
+//					System.out.println(tempString);
+					
+					// CONVERT ',' INTO ';'
+					StringBuilder paperTitle = new StringBuilder(tempString);
+					int len2 =tempString.length();
+					for(int j=0 ; j<len2 ; j++) {
+						if(tempString.charAt(j) == ',') paperTitle.setCharAt(j, ';');
+					}
+
+					newScopus.setPaperTitle(paperTitle.toString());
 				}
 				// Indexed By & Publication Type
 				if(inputLine.contains("indexed-by")){
+					// COMBINE ALL DATA INTO ONE SINGLE STRING
 					tempString = "";
 					inputLine = pageResult.readLine().trim();
 					tempString = tempString + inputLine;
@@ -72,25 +106,52 @@ public class GetScopusDataOnSINTA {
 					endIndex = tempString.indexOf("<");
 					tempString = tempString.substring(startIndex, endIndex).trim();
 					
+					// CONVERT '|' INTO ';'
+					StringBuilder paperDetail = new StringBuilder(tempString);
 					int len = tempString.length();
 					for(int i=0 ; i<len ; i++) {
-						if(tempString.charAt(i) == '|') {
-							endIndex = i;
-						}
+						if(tempString.charAt(i) == '|') paperDetail.setCharAt(i, '@');
 					}
 					
-					// Indexed By
-					String indexedBy;
-					indexedBy = tempString.substring(startIndex, endIndex).trim();
-					System.out.println(indexedBy);
-					
-					// Publication Type
-					String publicationType;
-					publicationType = tempString.substring(endIndex+2, len).trim();
-					System.out.println(publicationType);
+					// SPLIT EACH DETAIL INTO SINGLE STRING
+					tempString = paperDetail.toString();					
+					String[] data = tempString.split("@");
+					for (int i=0; i<data.length; i++) {
+						switch(i) {
+							// indexed by
+							case 0: // CONVERT ',' INTO ';'
+									StringBuilder paperTitle = new StringBuilder(data[0].trim());
+									int len2 = data[0].trim().length();
+									for(int j=0 ; j<len2 ; j++) {
+										if(tempString.charAt(j) == ',') paperTitle.setCharAt(j, ';');
+									}
+
+//									System.out.println(paperTitle.toString());
+									newScopus.setPublicationsName(paperTitle.toString());
+									break;
+							// volume
+							case 1: newScopus.setVolume(data[1].substring(data[1].indexOf(":")+2).trim());
+//									System.out.println(data[1].substring(data[1].indexOf(":")+2).trim());
+									break;
+							// issue
+							case 2: newScopus.setIssue(data[2].substring(data[2].indexOf(":")+2).trim());
+//									System.out.println(data[2].substring(data[2].indexOf(":")+2).trim());
+									break;
+							// date of publish
+							case 3: newScopus.setDate(data[3].trim());
+//									System.out.println(data[3].trim());
+									break;							
+							// publication type
+							case 4: newScopus.setPublicationsJournal(data[4].trim());
+//									System.out.println(data[4].trim());
+									break;
+						}
+					}
 				}				
 			}
+	        pageResult.close();
     	}    	
+		fileWriter.close();
 	}
 
 	private static int getEndPage(int universityID) throws Exception {
